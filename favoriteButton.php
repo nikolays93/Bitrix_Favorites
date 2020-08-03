1465b77a6c89c7aa92b123f8a13d7e7f61892c12
NikolayS93 <nikolays93@ya.ru>
2020-08-03 12:39:36 +0400

    Fix codestyle by phpstrom setting up as https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=43&LESSON_ID=3095&LESSON_PATH=3913.4776.4640.3095

--- favoriteButton.php
+++ favoriteButton.php
@@ -1,45 +1,46 @@
-<?
+<?php
 
 use Bitrix\Main\Application;
 use Bitrix\Main\Web\Cookie;
-$application = Application::getInstance();
-$context = $application->getContext(); 
- /* Вывод количества избранного */
-    if(!$USER->IsAuthorized()) // Для неавторизованного
-    {
-        $arElements = unserialize($APPLICATION->get_cookie('favorites'));
-        if($arElements == '')
-		unset($arElements);
 
-	foreach($arElements as $k=>$fav) // Checking empty IDs
-	{
-		if($fav == '0')
-			 unset($arElements[$k]);
-			unset($fav);
+$application = Application::getInstance();
+$context     = $application->getContext();
+/* Вывод количества избранного */
+if ( ! $USER->IsAuthorized()) // Для неавторизованного
+{
+	$arElements = unserialize($APPLICATION->get_cookie('favorites'));
+	if ($arElements == '') {
+		unset($arElements);
 	}
-        $wishCount = count($arElements);
-    }
-    else {
-         $idUser = $USER->GetID();
-         $rsUser = CUser::GetByID($idUser);
-         $arUser = $rsUser->Fetch();
-        foreach($arUser['UF_FAVORITES'] as $k=>$fav) // Checking empty IDs
+
+	foreach ($arElements as $k => $fav) // Checking empty IDs
 	{
-		if($fav == '0') 
-		{
+		if ($fav == '0') {
+			unset($arElements[$k]);
+		}
+		unset($fav);
+	}
+
+	$wishCount = count($arElements);
+} else {
+	$idUser = $USER->GetID();
+	$rsUser = CUser::GetByID($idUser);
+	$arUser = $rsUser->Fetch();
+	foreach ($arUser['UF_FAVORITES'] as $k => $fav) // Checking empty IDs
+	{
+		if ($fav == '0') {
 			unset($arUser['UF_FAVORITES'][$k]);
 			unset($fav);
 		}
 	}
-        $wishCount = count($arUser['UF_FAVORITES']);
 
+	$wishCount = count($arUser['UF_FAVORITES']);
+}
 
-     }
-
- /* Вывод количества избранного */
+/* Вывод количества избранного */
 ?>
 <a id='want' class="block" href="/personal/wishlist/">
-    <span class="col"><?=$wishCount?></span>
+    <span class="col"><?= $wishCount ?></span>
     <div class="icon"></div>
     <p>Хочу</p>
 </a>
